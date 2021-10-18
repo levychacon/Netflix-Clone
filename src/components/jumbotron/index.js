417fd7cc72e5aa8e2edbb7bfc6498cdbd00697fa
Levy Chacon <levychacon@yahoo.com>
2021-10-18 08:16:15 -0300

    Global styles added

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,33 +1,40 @@
-import React from 'react'
-import { Container,Inner, Pane, Title, SubTitle, Image } from './jumbotron.styles'
+import React from "react";
+import {
+  Container,
+  Inner,
+  Item,
+  Pane,
+  Title,
+  SubTitle,
+  Image,
+} from "./jumbotron.styles";
 
-export const Jumbotron = ({children, direction="row", ...restProps}) => {
-    return (
-     <Inner direction={direction}>
-       {children}
-     </Inner>
-    )
-}
+export const Jumbotron = ({ children, direction = "row", ...restProps }) => {
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  );
+};
 
-Jumbotron.Container = function JumbotronContainer({children,...restProps}){
-    return <Container {...restProps}>{children}</Container>
-}
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
 
-Jumbotron.Pane = function JumbotronPane({children,...restProps}){
-    return <Pane {...restProps}>{children}</Pane>
-}
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
 
-Jumbotron.Title = function JumbotronTitle({children,...restProps}){
-    return <Title {...restProps}>{children}</Title>
-}
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
 
-Jumbotron.SubTitle = function JumbotronSubTitle({children,...restProps}){
-    return <SubTitle {...restProps}>{children}</SubTitle>
-}
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
 
-Jumbotron.Image = function JumbotronImage({...restProps}){
-    return <Image {...restProps}/>
-}
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+  return <Image {...restProps} />;
+};
 
-
-export default Jumbotron
+export default Jumbotron;
